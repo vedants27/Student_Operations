e37b54cbeb7892028e5bcedc4a1d932d06a500ad
vedants27 <vedantss27@gmail.com>
2025-03-05 23:10:41 +0530

    Added StudentOperations class and constructor.

--- StudentOperations.java
+++ StudentOperations.java
@@ -1,1 +1,12 @@
+//StudentOperations.java
+
+import java.util.*;
+
+class StudentOperations {
+    ArrayList<Student> students; // ArrayList to store Student objects
+
+    public StudentOperations() {
+        this.students = new ArrayList<>();
+    }
+}
 
