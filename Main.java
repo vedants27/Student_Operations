7fc29fda1812d89878eb5b8ff4767eff2e1d6611
vedants27 <vedantss27@gmail.com>
2025-03-05 23:03:21 +0530

    Added Add Student and Display Students options.

--- Main.java
+++ Main.java
@@ -32,6 +32,27 @@ class Main {
                 break;
             }
 
+            switch (choice) {
+                case 1: // Add Student
+                    System.out.print("Enter Name: ");
+                    String name = scan.nextLine();
+                    System.out.print("Enter PRN: ");
+                    long prn = Long.parseLong(scan.nextLine());
+                    System.out.print("Enter Branch: ");
+                    String branch = scan.nextLine();
+                    System.out.print("Enter Batch: ");
+                    String batch = scan.nextLine();
+                    System.out.print("Enter CGPA: ");
+                    double cgpa = Double.parseDouble(scan.nextLine());
+
+                    Student student = new Student(name, prn, branch, batch, cgpa);
+                    operations.addStudents(student);
+                    break;
+                
+                case 2: // Display Students
+                    operations.displayStudents();
+                    break;
+
 
 
     }
