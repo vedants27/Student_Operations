5d714534e78195c8528c0a5cd5a5a960bb0e1b0f
vedants27 <vedantss27@gmail.com>
2025-03-05 22:58:08 +0530

    Added Main class and main method

--- Main.java
+++ Main.java
@@ -1,1 +1,13 @@
+//Name: Vedant Shitole 
+//PRN: 23070126143
+//Batch: AIML B3
+
+//Main.java
+
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in); // To take user input
+    }
+}
 
